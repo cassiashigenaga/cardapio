0a9fc65f1b2b6cbc0bd0dd4423fbd6379530dc96
cassia.mshigenaga <CassiaMiwa92>
2019-04-25 09:54:48 -0300

    Desenvolvimento da classe produtos e adição de ícone

--- app/src/main/java/com/example/cassiamshigenaga/cardapio/Cardapio2.java
+++ app/src/main/java/com/example/cassiamshigenaga/cardapio/Cardapio2.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.cassiamshigenaga.cardapio.modelos.Produto;
+
 
 public class Cardapio2 extends AppCompatActivity {
+
+    public Produto produto1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +19,25 @@ public class Cardapio2 extends AppCompatActivity {
 
         Intent intent = getIntent();
         String titulo = intent.getStringExtra(MainActivity.TITULO);
-        String item1 = intent.getStringExtra(MainActivity.ITEM1);
-        String item2 = intent.getStringExtra(MainActivity.ITEM2);
+
         TextView textViewTitulo = findViewById(R.id.tvTitulo);
-        TextView textViewItem1 = findViewById(R.id.tvItem1);
-        TextView textViewItem2 = findViewById(R.id.tvItem2);
         textViewTitulo.setText(titulo);
-        textViewTitulo.setText(item1);
-        textViewTitulo.setText(item2);
+
+        TextView textViewItem1 = findViewById(R.id.tvItem1);
+        TextView textViewDesc1 = findViewById(R.id.tvDesc1);
+        TextView textViewPreco1 = findViewById(R.id.tvPreco1);
+
+        if (titulo.equals("Beringela")){
+            produto1 = new Produto("Antepasto", 12.99, "Berinjela", "Berinjela, pimentão, arroz");
+            textViewItem1.setText(produto1.getNome());
+            textViewDesc1.setText(produto1.getDescricao());
+            textViewPreco1.setText("R$" + produto1.getPreco().toString());
+
+        }
+
+        else if (titulo.equals("Hamburguer")){
+            textViewItem1.setText("X-Salada");
+        }
 
     }
 }
--- app/src/main/java/com/example/cassiamshigenaga/cardapio/MainActivity.java
+++ app/src/main/java/com/example/cassiamshigenaga/cardapio/MainActivity.java
@@ -7,12 +7,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TITULO = "cam.example.cardapio.MENSAGEMTITULO";
-    public static final String ITEM1 = "cam.example.cardapio.MENSAGEMITEM1";
-    public static final String ITEM2 = "cam.example.cardapio.MENSAGEMITEM2";
+    public static final String TITULO = "com.example.cardapio.MENSAGEMTITULO";
     public String tituloDaActivity;
-    public String textoItem1;
-    public String textoItem2;
 
 
     @Override
@@ -23,46 +19,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void botaoHamburguer(View view) {
         tituloDaActivity = "Hamburguer";
-        textoItem1 = "X-Salada";
-        textoItem2 = "X-Bacon";
         Intent intent = new Intent(this, Cardapio2.class);
         intent.putExtra(TITULO, tituloDaActivity);
-        intent.putExtra(textoItem1, tituloDaActivity);
-        intent.putExtra(textoItem2, tituloDaActivity);
-
         startActivity(intent);
     }
 
     public void botaoPratosQuentes(View view) {
         tituloDaActivity = "Beringela";
-        textoItem1 = "Sopa";
-        textoItem2 = "Arroz";
         Intent intent = new Intent(this, Cardapio2.class);
         intent.putExtra(TITULO, tituloDaActivity);
-        intent.putExtra(textoItem1, tituloDaActivity);
-        intent.putExtra(textoItem2, tituloDaActivity);
         startActivity(intent);
     }
 
     public void botaoBebidas(View view) {
         tituloDaActivity = "Drinks";
-        textoItem1 = "Cerveja";
-        textoItem2 = "Caipirinha";
         Intent intent = new Intent(this, Cardapio2.class);
         intent.putExtra(TITULO, tituloDaActivity);
-        intent.putExtra(textoItem1, tituloDaActivity);
-        intent.putExtra(textoItem2, tituloDaActivity);
         startActivity(intent);
     }
 
     public void botaoDoces(View view) {
         tituloDaActivity = "Doces";
-        textoItem1 = "Brigadeiro";
-        textoItem2 = "Bolo";
         Intent intent = new Intent(this, Cardapio2.class);
         intent.putExtra(TITULO, tituloDaActivity);
-        intent.putExtra(textoItem1, tituloDaActivity);
-        intent.putExtra(textoItem2, tituloDaActivity);
         startActivity(intent);
     }
 }
